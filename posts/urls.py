bc5edbb153de4ae6f2e2dad9b36c6d58c1816d81
je-ss-y <Nkujess@gmail.com>
2019-10-18 11:12:20 +0200

    added media root url to posts url

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -7,3 +9,6 @@ urlpatterns=[
     url('',views.posts_of_day,name='postsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastposts')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
