b9c2daca616b929f67ac7a10a15c891955d5b7c7
je-ss-y <Nkujess@gmail.com>
2019-10-18 11:09:00 +0200

    added media root

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Image(models.Model):
+    image=  models.ImageField(upload_to='images/', blank=True)
+    photoname = models.TextField()
+    caption = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # tags = models.ManyToManyField(tags)
+
+
+    def __str__(self):
+        return self.photoname
+    
+  
