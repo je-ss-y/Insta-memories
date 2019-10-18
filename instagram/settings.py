1d52c2be09858cb1392c64b2c3359e6e7d334e77
je-ss-y <Nkujess@gmail.com>
2019-10-18 09:57:42 +0200

    changed database configurations in settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'memories',
+        'USER': 'wecode',
+    'PASSWORD':'passiwadi',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
